3e844990891e3628fd2443d47d22c899887e9e4a
kangyeongmin <kym6050@hanyang.ac.kr>
2022-12-16 21:27:45 +0900

    크루 정보 ui 출력 로직 수정

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -8,11 +8,13 @@ const OutputView = {
     MissionUtils.Console.print(
       "\n###############################################"
     );
-    MissionUtils.Console.print("과정: 백엔드 | 프론트엔드");
+    MissionUtils.Console.print("과정: " + CrewInfo.course.join(" | "));
     MissionUtils.Console.print("미션:");
-    MissionUtils.Console.print(
-      "   - 레벨1: 자동차경주 | 로또 | 숫자야구게임\n   - 레벨2: 장바구니 | 결제 | 지하철노선도\n   - 레벨3:\n   - 레벨4: 성능개선 | 배포\n   - 레벨5: "
-    );
+    CrewInfo.level.forEach((level, index) => {
+      MissionUtils.Console.print(
+        "   - " + level + ": " + CrewInfo.mission[index].join(" | ")
+      );
+    });
     MissionUtils.Console.print(
       "###############################################"
     );
